eefbd7ed56fdeb12d70de4424632341ed7aa5de5
ch-Usama096 <usamahusnain096@gmail.com>
2024-11-14 18:12:30 +0500

    Add the Code for the Model Development

--- Practice_Folder/fraud_detection/main.py
+++ Practice_Folder/fraud_detection/main.py
@@ -1,1 +1,42 @@
 
+# Import the Important Modules & Functions
+from model_monitering import read_dataset , is_dataset_balance , convert_matrix , preprocessing , split_dataset , model_development
+
+
+
+
+# Run the Main  (For the Runing Project)
+if __name__ == "__main__":
+    
+    # Define the DATASET PATH
+    DATASET_PATH = "../Dataset/fraud_detection_dataset.csv"
+    dataset      = read_dataset(DATASET_PATH)
+    
+    # Display the Count of the Class Label
+    firstCount , secondCount = is_dataset_balance(dataset)
+    # # Display the Count of the First & Second Class Label
+    # print(f"\nHere is the Count of First Class Label  : {firstCount}")
+    # print(f"Here is the Count of Second Class Label : {secondCount}")
+
+    # Convert the Dataset into X & y Numpy Array Matrix
+    X , y = convert_matrix(dataset)
+    # # Display the Shape of X & y Matrix
+    # print(f"\nHere is the Shape of the X Matrix : {X.shape}")
+    # print(f"Here is the Shape of the Y Matrix : {y.shape}")
+
+    # Pre-processing the Dataset
+    X , y = preprocessing(X , y)
+
+    # Split the Dataset into Training & Testing 
+    x_train , y_train , x_test , y_test = split_dataset(X , y)
+    # # Display the Shape of the Training and Testing 
+    # print(f"Here is the Shape of the X Train : {x_train.shape}")
+    # print(f"Here is the Shape of the Y Train : {y_train.shape}")
+    # print(f"Here is the Shape of the X Test  : {x_test.shape}")
+    # print(f"Here is the Shape of the Y Test  : {y_test.shape}")
+
+    # Development the Model in the Training & Testing Dataset
+    models_acc = model_development(x_train , y_train , x_test , y_test)
+    # Display the Accuracy
+    print(f"Here is the Accuracy of the Models : {models_acc}")
+
--- Practice_Folder/fraud_detection/model_monitering.py
+++ Practice_Folder/fraud_detection/model_monitering.py
@@ -1,1 +1,128 @@
-"" 
+# Import the Important Modules
+import numpy as np
+import pandas as pd
+import warnings
+from sklearn.exceptions import DataConversionWarning
+from sklearn.preprocessing import OneHotEncoder , LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+
+# Ingore the Warnings
+warnings.simplefilter(action='ignore', category=DataConversionWarning)
+
+
+# Create the Fucntions for the Reading Dataset
+def read_dataset(DATASET_PATH):
+    # Read the Dataset 
+    dataset = pd.read_csv(DATASET_PATH)
+
+    # Return the Dataset
+    return dataset
+
+
+# Create the Function for Finding the Information about the Dataset Such as (Shape , Missing Values , Duplictaed Data)
+def info_dataset(dataset):
+    # Display the Shape of the Dataset
+    print(f"\nHere is the Shape of the Dataset : {dataset.shape}")
+
+    # Check the Missing Values in the Dataset
+    missingValues = dataset.isnull().sum()
+    print(f"Here is the Missing Values of each Colm : \n{missingValues}")
+
+    # Check the Duplicated Row in the Dataset
+    duplicatedRow = dataset.duplicated().sum()
+    print(f"Here is the Total Duplicated Row in the Dataset : {duplicatedRow}")
+
+
+# Create the Function for checking the Balance or Not Balance Dataset
+def is_dataset_balance(dataset):
+    # Get the Count of Each Class Label
+    countFirst , countSecond = dataset["Fraud"].value_counts()
+
+    # # Display the Count of the First & Second Class Label
+    # print(f"Here is the Count of First Class Label  : {countFirst}")
+    # print(f"Here is the Count of Second Class Label : {countSecond}")
+
+    # Return the Count Label
+    return countFirst , countSecond
+
+
+# Create the Function for Conmverting the Dataset into Dependent & Independent Matrix
+def convert_matrix(dataset):
+    # Convert the Dataset into Dependent & Independent Matrix
+    X = dataset.iloc[:,:-1].values
+    y = dataset.iloc[:,-1].values
+
+    # # Display the Shape of X & y Matrix
+    # print(f"Here is the Shape of the X Matrix : {X.shape}")
+    # print(f"Here is the Shape of the Y Matrix : {y.shape}")
+
+    # Return the X & y Matrix
+    return X , y
+
+
+# Create the Function For the Preprocessing of the Dataset
+def preprocessing(X , y):
+    # Preprocessing the Dataset
+
+    # Convert the Categorical Data into Numerical Data
+    encoder   = LabelEncoder() # Create the Object of LabelEncoder
+    cat_index = [0 , 3] 
+    for index in cat_index:
+        X[:,[index]] = encoder.fit_transform(X[:,[index]]).reshape(-1,1)
+    
+    
+    # Normalize the Data (Convert Numerical Data into Range (0-1))
+    scaler = MinMaxScaler()
+    X = scaler.fit_transform(X)
+
+    # Return the X & y
+    return X , y
+
+
+# Create the Function for Splitting the Dataset into Training & Testing
+def split_dataset(X , y):
+    # Split the Dataset into Training & Testing 
+    x_train , y_train , x_test , y_test = train_test_split(X , y , test_size = 0.25 , random_state = 42)
+
+    # # Display the Shape of the Training and Testing 
+    # print(f"Here is the Shape of the X Train : {x_train.shape}")
+    # print(f"Here is the Shape of the Y Train : {y_train.shape}")
+    # print(f"Here is the Shape of the X Test  : {x_test.shape}")
+    # print(f"Here is the Shape of the Y Test  : {y_test.shape}")
+
+    # Return the training and Testing Data
+    return x_train , y_train , x_test , y_test
+
+
+# Create the Functions for Appling the Model
+def model_development(x_train , y_train , x_test , y_test):
+    # Create the Object of the Models
+    lg = LogisticRegression()
+    dt = DecisionTreeClassifier()
+
+    # Create the List for Define the Model Name
+    models = [lg , dt]
+
+    # Craete the List for Stroing the model
+    models_acc = []
+    for model in models:
+        # Fit the Model in the Train Dataset for Training the Model
+        model.fit(x_train , x_test)
+
+        # Predict the Model in the Testing Dataset fro Checking the Model
+        prediction = model.predict(y_train)
+
+        # Calculate the Accuracy of the Model
+        acc = accuracy_score(y_test , prediction)
+
+        # Store the Accuracy in the List
+        models_acc.append(acc)
+
+    # Return the Models Accuracy
+    return models_acc
+
